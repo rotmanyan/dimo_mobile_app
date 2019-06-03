ec409543acdf7ead3fb88781b821b8ad5566130e
Rotman Yan <rotman.yan@gmail.com>
2019-06-03 16:19:06 +0300

    nav test error

---
+++ app/views/testCamera/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TestCamera extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default TestCamera;
