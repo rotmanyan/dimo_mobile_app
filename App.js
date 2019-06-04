9cf5542d143868474f776619285292fae0aea4a8
Rotman Yan <rotman.yan@gmail.com>
2019-06-04 14:22:30 +0300

    install react-native-camera

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SignUser from './app/views/signUser'
+import OnBoard from './app/views/onBoard'
 // import ScanScreen from './app/views/testCamera'
-import ExampleApp from './app/views/testContacts'
+// import ExampleApp from './app/views/testContacts'
 import {StyledView} from './stylesApp'
 
 export const App = () => (
   <StyledView>
-    <SignUser/>
+    {/*<SignUser/>*/}
+    <OnBoard/>
     {/*<ScanScreen/>*/}
     {/*<ExampleApp/>*/}
   </StyledView>
---
+++ app/views/onBoard/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import {MainView, ButtonView, StyledButton, TextHeadMiddle} from "./styles";
+
+class OnBoard extends Component {
+  render() {
+    return (
+      <MainView>
+        <TextHeadMiddle>
+          On board
+        </TextHeadMiddle>
+      </MainView>
+    );
+  }
+}
+
+export default OnBoard
