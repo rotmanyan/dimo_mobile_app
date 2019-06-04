4f92bbefcff4a8d0ed38aac3e862d0d72ef1d137
Rotman Yan <rotman.yan@gmail.com>
2019-06-04 18:26:26 +0300

    go for svg

---
+++ app/views/onBoard/body/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native'
+
+export const MainView = styled.View`
+  justify-content: flex-end;
+`
+
+export const View = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #fff;
+  align-items: center;
+`
+
+//button
+export const ButtonView = styled.View`
+  background-color: #fff;
+  height: 56px;
+`
+
+export const Button = styled.Button`
+`
