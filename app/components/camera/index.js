bc826dde686c510ba91fe718b47d61f1f87570d3
Rotman Yan <rotman.yan@gmail.com>
2019-06-09 20:58:27 +0300

    qrcode scanner work

--- app/components/camera/index.js
+++ app/components/camera/index.js
@@ -1,79 +1,137 @@
 import React, {Component} from 'react';
-import {MainView, Button, Text, Image, CamStyle} from './styles'
-import {CameraKitCamera, CameraKitGalleryView} from 'react-native-camera-kit'
-import Contacts from 'react-native-contacts';
+import {TouchableOpacity, View, Text, Linking, StyleSheet} from 'react-native';
+// import {MainView, Button, Text, Image, CamStyle} from './styles'
+import {CameraKitCamera, CameraKitCameraScreen} from 'react-native-camera-kit'
 
 class Camera extends Component {
   state = {
     status: false,
-    url: ''
+    url: '',
+    QR_Code_Value: '',
+    Start_Scanner: false
   }
 
   componentDidMount() {
-    console.log(Contacts, 'Contacts');
-    /*    Contacts.getAll((err, contacts) => {
-          console.log(err, 'contacts');
-          console.log(contacts, 'contacts');
-          if (err) {
-            throw err;
-          }
-        })*/
+
   }
 
   capture = () => {
     const {url} = this.state
     !!url
       ? this.setState({url: ''})
-      : this.camera.capture(false)
-        .then(data => this.setState({url: data.uri}))
+      : this.camera.capture(true).then(data => this.setState({url: data.uri}))
+  }
+  openLink_in_browser = () => {
+    Linking.openURL(this.state.QR_Code_Value);
   }
 
-  changeCamera = () => {
-    const {url} = this.state
-    !!url
-      ? this.setState({url: ''})
-      : this.camera.changeCamera().then(data => console.log(data, 'changeCamera'))
+  onQR_Code_Scan_Done = (QR_Code) => {
+    this.setState({QR_Code_Value: QR_Code});
+    this.setState({Start_Scanner: false});
   }
-  onFlash = () => {
-    const {url} = this.state
-    !!url
-      ? this.setState({url: ''})
-      : this.camera.setFlashMode().then(data => console.log(data, 'onFlash'))
+
+
+  open_QR_Code_Scanner = () => {
+    this.setState({QR_Code_Value: ''});
+    this.setState({Start_Scanner: true});
   }
 
   render() {
-    const {url} = this.state
+    if (!this.state.Start_Scanner) {
+
+      return (
+        <View style={styles.MainContainer}>
+
+          <Text style={{fontSize: 22, textAlign: 'center'}}>React Native Scan QR Code Example</Text>
+
+          <Text style={styles.QR_text}>
+            {this.state.QR_Code_Value ? 'Scanned QR Code: ' + this.state.QR_Code_Value : ''}
+          </Text>
+
+          {this.state.QR_Code_Value.includes("http") ?
+            <TouchableOpacity
+              onPress={this.openLink_in_browser}
+              style={styles.button}>
+              <Text style={{color: '#FFF', fontSize: 14}}>Open Link in default Browser</Text>
+            </TouchableOpacity> : null
+          }
+
+          <TouchableOpacity
+            onPress={this.open_QR_Code_Scanner}
+            style={styles.button}>
+            <Text style={{color: '#FFF', fontSize: 14}}>
+              Open QR Scanner
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+      );
+    }
     return (
-      <MainView>
-        <Button onPress={this.capture}>
-          <Text>
-            Capture
-          </Text>
-        </Button>
-        <Button onPress={this.changeCamera}>
-          <Text>
-            Change camera
-          </Text>
-        </Button>
-        <Button onPress={this.onFlash}>
-          <Text>
-            Flash on
-          </Text>
-        </Button>
-        <CameraKitGalleryView
-          ref={gallery => this.gallery = gallery}
-          style={{flex: 1, marginTop: 20}}
-          minimumInteritemSpacing={10}
-          minimumLineSpacing={10}
-          columnCount={3}
-          onTapImage={event => {
-            console.log(event.nativeEvent, 'event');
-            // event.nativeEvent.selected - ALL selected images ids
-          }}
+      <View style={{flex: 1}}>
+
+        <CameraKitCameraScreen
+          showFrame={true}
+          scanBarcode={true}
+          laserColor={'#FF3D00'}
+          frameColor={'#00C853'}
+          colorForScannerFrame={'black'}
+          onReadCode={event =>
+            this.onQR_Code_Scan_Done(event.nativeEvent.codeStringValue)
+          }
         />
-      </MainView>
+
+      </View>
     );
   }
+
+  /*  render() {
+      const {url} = this.state
+      return (
+        <MainView>
+          <Button onPress={this.capture}>
+            <Text>
+              Capture
+            </Text>
+          </Button>
+          {!!url
+            ? <Image source={{uri: url}}/>
+            : <CameraKitCamera
+              ref={cam => this.camera = cam}
+              style={CamStyle}
+              cameraOptions={{
+                focusMode: 'on',               // off/on(default)
+                ratioOverlayColor: 'transparent' // optional
+              }}
+              onReadQRCode={(event) => console.log(event.nativeEvent.qrcodeStringValue)} // optional
+
+            />}
+        </MainView>
+      );
+    }*/
 }
+
+const styles = StyleSheet.create({
+
+  MainContainer: {
+    flex: 1,
+    paddingTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  QR_text: {
+    color: '#000',
+    fontSize: 19,
+    padding: 8,
+    marginTop: 12
+  },
+  button: {
+    backgroundColor: '#2979FF',
+    alignItems: 'center',
+    padding: 12,
+    width: 300,
+    marginTop: 14
+  },
+});
 
 export default Camera
