afc07885910e8e68c9927fc2260bb0e4046d8326
Rotman Yan <rotman.yan@gmail.com>
2019-07-04 12:01:43 +0300

    begin enter password

--- app/views/startPage/index.js
+++ app/views/startPage/index.js
@@ -1,27 +1,28 @@
-import React, {Component} from "react";
-import {ActivityIndicator, View, Text, TouchableHighlight} from "react-native";
-import AsyncStorage from "@react-native-community/async-storage";
-import {connect} from "react-redux";
-import {getLanguages} from "react-native-i18n";
+import React, {Component} from 'react'
+import {ActivityIndicator, View, Text, TouchableHighlight} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import {connect} from 'react-redux'
+import {getLanguages} from 'react-native-i18n'
 import {
   setLocalizationRequest,
   setLocalizationSuccess
-} from "../../services/i18n/actions";
+} from '../../services/i18n/actions'
 import {
   createAppContainer,
   createStackNavigator,
   createBottomTabNavigator,
   DrawerNavigator,
-} from "react-navigation";
-import Kyc from "../kyc";
-import Chat from "../chat";
-import Wallet from "../wallet";
-import Profile from "../profile";
-import FirstScreen from "../firstScreen";
-import Activity from "../activity";
-import Send from "../send";
-import SignUser from "../signUser";
-import PersonalChat from "../../components/personalChat";
+} from 'react-navigation'
+import Kyc from '../kyc'
+import Chat from '../chat'
+import Wallet from '../wallet'
+import Profile from '../profile'
+import FirstScreen from '../firstScreen'
+import Activity from '../activity'
+import Send from '../send'
+import SignUser from '../signUser'
+import PersonalChat from '../../components/personalChat'
+import EnterPassword from '../../components/enterPassword'
 
 const ChatStack = createStackNavigator(
   {
@@ -29,18 +30,18 @@ const ChatStack = createStackNavigator(
     PersonalChat,
   },
   {
-    initialRouteName: "Chat"
+    initialRouteName: 'Chat'
   }
-);
+)
 
 const SendStack = createStackNavigator(
   {
     Send
   },
   {
-    initialRouteName: "Send"
+    initialRouteName: 'Send'
   }
-);
+)
 
 const WalletStack = createStackNavigator(
   {
@@ -48,18 +49,18 @@ const WalletStack = createStackNavigator(
     First: FirstScreen,
   },
   {
-    initialRouteName: "First"
+    initialRouteName: 'First'
   }
-);
+)
 
 const ActivityStack = createStackNavigator(
   {
     Activity
   },
   {
-    initialRouteName: "Activity"
+    initialRouteName: 'Activity'
   }
-);
+)
 
 const ProfileStack = createStackNavigator(
   {
@@ -67,20 +68,20 @@ const ProfileStack = createStackNavigator(
     Kyc
   },
   {
-    initialRouteName: "Profile"
+    initialRouteName: 'Profile'
   }
-);
+)
 
 ProfileStack.navigationOptions = ({navigation}) => {
-  let tabBarVisible = true;
+  let tabBarVisible = true
   if (navigation.state.index > 0) {
-    tabBarVisible = false;
+    tabBarVisible = false
   }
 
   return {
     tabBarVisible
-  };
-};
+  }
+}
 
 /*
 class LogoTitle extends Component {
@@ -92,7 +93,7 @@ class LogoTitle extends Component {
           style={{width: 30, height: 30}}
         />
       </ViewLogo>
-    );
+    )
   }
 }
 */
@@ -130,7 +131,7 @@ class GreenScreen extends Component {
           <Text style={{color: '#40ff00'}}>Go to Red</Text>
         </TouchableHighlight>
       </View>
-    );
+    )
   }
 }
 
@@ -144,7 +145,7 @@ class RedScreen extends Component {
           <Text style={{color: '#ff0000'}}>Back to Green</Text>
         </TouchableHighlight>
       </View>
-    );
+    )
   }
 }
 
@@ -159,52 +160,53 @@ const Navigation = createAppContainer(
       Profile: ProfileStack,
     },
     {
-      initialRouteName: "Profile",
+      initialRouteName: 'Profile',
       tabBarOptions: {
-        activeTintColor: "#3878FF",
-        inactiveTintColor: "#90a5c2"
+        activeTintColor: '#3878FF',
+        inactiveTintColor: '#90a5c2'
       },
       defaultNavigationOptions: {
         headerStyle: {
-          backgroundColor: "#3878FF"
+          backgroundColor: '#3878FF'
         },
-        headerTintColor: "#fff",
+        headerTintColor: '#fff',
         headerTitleStyle: {
-          fontWeight: "bold",
-          color: "#fff"
+          fontWeight: 'bold',
+          color: '#fff'
         }
       }
     }
   )
-);
+)
 
 class StartPage extends Component {
   state = {
     isLoading: true,
-    isAuthenticated: false
-  };
+    isAuthenticated: false,
+    phone: false
+  }
 
   componentDidMount() {
-    getLanguages()
-      .then(data => setLocalizationSuccess(data[0].split('-')[0]))
-    /*   AsyncStorage.getItem('phone')
-         .then(phone => {
+    this.setState({phone: false}, () => {
+      getLanguages()
+        .then(data => setLocalizationSuccess(data[0].split('-')[0]))
 
-         })*/
-
-    AsyncStorage.getItem('token')
-      .then(token => {
-        if (token || this.props.token) setTimeout(() => this.setState({isAuthenticated: true}, () => this.setState({isLoading: false})), 15)
-        else setTimeout(() => this.setState({isLoading: false}), 15)
-      })
+      AsyncStorage.getItem('phone')
+        .then(phone => {
+          this.setState({phone: !!phone, isAuthenticated: this.props.token})
+        })
+    })
   }
 
   render() {
-    if (this.state.isLoading) {
+    const {isLoading, phone, isAuthenticated} = this.state
+    console.log(this.state, 'phone state')
+
+    if (isLoading) {
       return <ActivityIndicator
-        animating={this.state.isLoading}
+        animating={isLoading}
         color='#3878FF'
-        size="large"
+        size='large'
         style={{
           flex: 1,
           justifyContent: 'center',
@@ -212,23 +214,34 @@ class StartPage extends Component {
           height: 100
         }}
       />
-    } else return this.state.isAuthenticated || this.props.token
-      ? <Navigation style={{backgroundColor: '#e9edf2'}}/>
-      : <SignUser/>
+    } else if (!isLoading) {
+      if (phone) {
+        if (isAuthenticated) {
+          return <Navigation style={{backgroundColor: '#e9edf2'}}/>
+        } else return <EnterPassword/>
+      } else if (!phone) {
+        return <SignUser/>
+      }
+    }
+    /* else return phone
+       ? isAuthenticated || token
+         ? <Navigation style={{backgroundColor: '#e9edf2'}}/>
+         : <SignUser/>
+       : <EnterPassword/>*/
   }
 }
 
 const MSTP = state => ({
   token: state.profile.token,
   localization: state.localization.language
-});
+})
 
 const MDTP = {
   setLocalizationRequest,
   setLocalizationSuccess
-};
+}
 
 export default connect(
   MSTP,
   MDTP
-)(StartPage);
+)(StartPage)
